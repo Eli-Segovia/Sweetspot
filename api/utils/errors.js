b99d1435e1d94f2d43737c165aa34a6244e2aea3
Eli <elisego46@gmail.com>
2022-09-19 10:11:55 -0500

    feature: remove redundancy and move http res to one file

--- api/utils/errors.js
+++
@@ -1,7 +0,0 @@
-import { HTTPErrorBuilder } from './HTTPError.js';
-
-export const unableToFindStoreErr = function (id) {
-    return new HTTPErrorBuilder()
-        .message(`Store not found with id of ${id}`)
-        .code(404);
-};
